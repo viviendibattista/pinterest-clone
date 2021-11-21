079711ac4c1a9a999d8bb8e8fca942e5e8002b50
Vivien Di Battista <vivien.di.battista@gmail.com>
2021-11-21 16:09:45 +0100

    Image upload works

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -6,15 +6,21 @@ use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => false
+            ])
             ->add('title')
-            ->add('description')
-        ;
+            ->add('description');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
